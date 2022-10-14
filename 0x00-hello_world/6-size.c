23bc3341d5a97eae99aba89b3b789ce9a46ab096
Shihabuddeen <113627363+Shihabuddeen@users.noreply.github.com>
2022-10-14 21:36:57 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,6 @@
-Size of a char: 1  byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-
-(145 chars long)
-[stderr]: 
-(0 chars long)
-[Expected]
 Size of a char: 1 byte(s)
 Size of an int: 4 byte(s)
 Size of a long int: 4 byte(s)
 Size of a long long int: 8 byte(s)
 Size of a float: 4 byte(s)
-
-(144 chars long)
-[stderr]: [Anything]
-(0 chars long)
+)
