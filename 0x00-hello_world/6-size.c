7660b235a8defe7e4d6dc5c1017ec6c171422a9d
Shihabuddeen <113627363+Shihabuddeen@users.noreply.github.com>
2022-10-14 21:15:34 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-/**
- * main - printing the sizes
- * Return: Always 0
- */
-int main(void)
-{
-	printf("Size of a char: %i  byte(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n",  sizeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return (0);
-}
+Size of a char: 1  byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+
+(145 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+
+(144 chars long)
+[stderr]: [Anything]
+(0 chars long)
