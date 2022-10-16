682b49550186e11a081cff1307598386bc53a5d9
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-15 23:07:55 -0700

    This is my territory

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-prints if the number is negative,positive or 0
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int n;
+
+	$rand(time(0));
+
