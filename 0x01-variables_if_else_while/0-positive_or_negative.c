2343cedfd072f5acc6aa9adda75252a538d95273
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-16 11:09:59 -0700

    Real madrid is my team

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,21 @@
 #include <time.h>
 
 /**
- * main-prints if the number is negative,positive or 0
- * Return: Always 0 (Successful)
+ * main - prints positive, negative or zero
+ *
+ * Return: 0 (Successful)
  */
 int main(void)
 {
 	int n;
 
-	$rand(time(0));
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
