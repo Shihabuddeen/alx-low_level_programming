d6b991cc2fd375f335dfcf3f3c536a87229179fe
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-17 06:56:49 -0700

    Betty one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - prints all combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+
+		putchar(x);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
+
