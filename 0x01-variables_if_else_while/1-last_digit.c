b4716aabc6f225c8a1e009788d4d3b86cb8e0729
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-17 05:32:13 -0700

    It shall be well

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
-
+	printf("Last digit of %d is %d ", n, a);
 	if (a > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n" , n, a);
+		pritf("and is greater than 5");
 	}
-	else if (a == 0)
+	if (a == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, a);
+		printf("and is 0");
 	}
-	else
+	if (a < 6 && a != 0);
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("and is less than 6 and not 0");
 	}
 	printf("\n");
 	return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in upper and lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = a ; i <= 'z'; i++)
+		putchar(char);
+	for (j = Z 'A'; j <= 'Z'; j++)
+		putchar(char);
+
+	putchar('\n');
+
+	return (0);
+}
