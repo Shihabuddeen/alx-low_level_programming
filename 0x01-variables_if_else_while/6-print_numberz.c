cfa8a61c0a97c3b44f57b76b6187a1b7446bb37f
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-17 06:19:58 -0700

    Betty you are good

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+
+/**
+ * main - prints single digits in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
+
