96bdcf8b4244999e50133544ded831c9ff8f3764
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-18 00:25:35 -0700

    Betty one

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,21 +10,21 @@ int main(void)
 
 	for (i = 0; i < 99; i++)
 	{
-	for (j = i + 1; j < 99; j++)
-	{
-		putchar('0' + i / 10);
-		putchar('0' + i % 10);
+		for (j = i + 1; j < 99; j++)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
 
-		putchar(' ');
+			putchar(' ');
 
-		putchar('0' + j / 10);
-		putchar('0' + j % 10);
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
 
-		if (i == 98 && j == 99)
-		break;
-		putchar(',');
-		putchar(' ');
-	}
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
