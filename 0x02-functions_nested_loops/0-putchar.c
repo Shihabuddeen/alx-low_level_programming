2115d48ff5da4e48a05f3348009b76669644c400
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-18 14:22:24 -0700

    Real

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by Shihab
+ * Return: 0
+ */
+int main(void)
+{
+	Puitchar('_');
+	Putchar('P');
+	Putchar('u');
+	Putchar('t');
+	Putchar('c');
+	Putchar('h');
+	Putchar('a');
+	Putchar('r');
+	Putchar('\n')
+	return (0);
+}
