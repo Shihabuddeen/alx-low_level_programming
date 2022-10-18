13bcfe1c326e7f247ac4207834e2b8e532c910ab
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-18 14:59:15 -0700

    Real

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-	Puitchar('_');
-	Putchar('P');
-	Putchar('u');
-	Putchar('t');
-	Putchar('c');
-	Putchar('h');
-	Putchar('a');
-	Putchar('r');
-	Putchar('\n')
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+
 }
