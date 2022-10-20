f555fd69e47c9bbaa6df49882d84db0ba8cae6d1
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-20 04:20:15 -0700

    Grit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * main - causes an infinite loop
@@ -12,7 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/*
+	/**
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
