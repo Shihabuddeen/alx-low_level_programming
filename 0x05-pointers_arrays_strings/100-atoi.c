3a8f4fbc631b826ba279cce5a6b10fac30e244d6
Shihabuddeen <shihabuddeenj@gmail.com>
2022-10-24 10:22:46 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,16 +13,13 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	do {
-
-		if (*s == '_')
+		if (*s == '-')
 			sign *= -1;
-
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
-
 		else if (num > 0)
 			break;
-	} while (*s++);
+		} while (*s++);
 
 	return (num * sign);
 }
